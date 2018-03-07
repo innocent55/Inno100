61f9693e8153079ee1461bc3842940d471200926
HAGENIMANA INNOCENT <innocenthagenimana5@gmail.com>
2018-03-07 13:31:08 +0000

    init

---
+++ db/schema.rb
@@ -0,0 +1,14 @@
+
+ActiveRecord::Schema.define(version: 20180305191831) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "blogs", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+  end
+
+end
+
+
